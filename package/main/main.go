21af82e5d161eed42185245a094fd20e75b1b919
RAJIB <rajib@RAJIBs-MacBook-Pro.local>
2018-07-29 22:37:20 +0600

    package example added

---
+++ package/main/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/rajib80/golearning/package/stringutil"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("jibra"))
+	fmt.Println(stringutil.MyName)
+}
