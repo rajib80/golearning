32a46b073b7348fd340b9b0682cf1652c1f5286e
RAJIB <rajib@RAJIBs-MacBook-Pro.local>
2018-08-13 21:44:58 +0600

    goroutine example 2 added

---
+++ goroutine/example2/goroutineexample2.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func say(msg string) {
+	for i := 0; i < 5; i++ {
+		time.Sleep(1000 * time.Millisecond)
+		fmt.Println(msg)
+	}
+}
+
+func main() {
+	go say("world")
+	say("hello")
+}
