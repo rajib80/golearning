13a79b6253cccbd7b52b476b8c864dd445520487
RAJIB <rajib@RAJIBs-MacBook-Pro.local>
2018-08-15 15:40:57 +0600

    Atomic Counters example added

---
+++ atomic-counter/atomic-counter.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"sync/atomic"
+	"time"
+)
+
+func main() {
+	var counter uint64
+
+	for i := 0; i < 50; i++ {
+		go func() {
+			for {
+				atomic.AddUint64(&counter, 1)
+				time.Sleep(time.Millisecond)
+			}
+		}()
+	}
+
+	time.Sleep(time.Second)
+
+	finalCounter := atomic.LoadUint64(&counter)
+	fmt.Println("ops:", finalCounter)
+}
